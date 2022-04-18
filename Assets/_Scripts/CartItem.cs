77a4d93de7b39795857e16491583598326b78fa2
SnagSovereign <joeysimon@outlook.com>
2022-04-18 21:19:23 +1000

    Can now increment/decrement and delete shopping cart items

---
+++ Assets/_Scripts/CartItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CartItem : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI quantityText;
+
+    private int itemId = -1;
+    private int quantity = 0;
+
+    public int GetItemId()
+    {
+        return itemId;
+    }
+
+    public void SetItemId(int id)
+    {
+        itemId = id;
+    }
+
+    public void IncrementQuantity()
+    {
+        quantity++;
+        UpdateUI();
+    }
+
+    public void DecrementQuantity()
+    {
+        if (quantity <= 0) return;
+        quantity--;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        quantityText.text = quantity.ToString();
+    }
+
+    public void DeleteCartItem()
+    {
+        Destroy(gameObject);
+    }
+}
